04aef1ff0f1429ed9562ac8bfb82e4ace5f11ccf
Evlen Fryling <evlen.fryling@gmail.com>
2025-01-17 18:19:44 -0500

    Task 1 complete

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+function addNumbers(a, b) {
+    return a + b;
+}
 
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+console.log(addNumbers(5, 10)); 
+console.log(multiplyNumbers(3, 4));  
+
+
+function calculateArea(length, width) {
+    return length * width;
+}
+
+console.log(calculateArea(5, 10));  
+
+
+function greet(name = "Guest", age = 25) {
+    console.log("Hello, " + name + "! You are " + age + " years old.");
+}
+
+greet();  
+greet("John", 30); 
+greet("Alice");  
+*/
